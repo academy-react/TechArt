16283f412a2e316d2ece60ca62d0fbe489a63c48
Cloner <aliabdi1108@gmail.com>
2023-10-02 19:10:10 +0330

    aded(fonts,SVGs and loginsEmty)

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "../Router/router";
 import "./App.css";
+import { SignUp } from "../components/Sign/SignUp/SignUp";
 
 function App() {
   return (

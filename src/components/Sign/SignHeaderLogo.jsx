8e6f5244fd96bdf5fcd629c8d4ca4104e1041d0c
Cloner <aliabdi1108@gmail.com>
2023-10-09 19:15:11 +0330

    responsive improve

--- src/components/Sign/SignHeaderLogo.jsx
+++ src/components/Sign/SignHeaderLogo.jsx
@@ -3,13 +3,13 @@ import Logo from "../../assets/icon/header-logo.png";
 
 const SignHeaderLogo = () => {
   return (
-    <>
+    <div className="mb-8 mx-auto">
       <img
-        className=" mb-8 mx-auto h-20 w-auto rounded-xl"
+        className="2xl:h-28 xl:h-20 lg:h-20 md:h-16 h-16 md w-auto rounded-xl"
         src={Logo}
         alt="Your Company"
       />
-    </>
+    </div>
   );
 };
 

036c88eb15caf9aca4a39700ad2e52e730de75e2
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-10-12 16:04:52 +0330

    test

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,8 +1,16 @@
-import { useState } from "react";
-import "./App.css";
+import React from "react";
+import { Landing } from "../screens/Landing/Landing";
 
-function App() {
-  return <></>;
-}
+//fonts
+
+const App = () => {
+  return (
+    <div className="	px-px border-2 border-red-600 w-auto	">
+      <div>
+        <Landing />
+      </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/screens/Landing/Landing.jsx
@@ -0,0 +1,15 @@
+import { Footer } from "../../components/common/Footer/Footer";
+import { NavbarHeader } from "../../components/common/NavbarHeader/NavbarHeader";
+import { HeroSection } from "../../components/HeroSection/HeroSection";
+
+function Landing() {
+  return (
+    <>
+      <NavbarHeader />
+      <HeroSection />
+      <Footer />
+    </>
+  );
+}
+
+export { Landing };

fb24b1f1a2f51e5328dc817e2d619cfd1e9e36cb
Cloner <aliabdi1108@gmail.com>
2023-10-16 13:38:39 +0330

    custom elements debug

--- src/components/common/customElements/CustomButton.jsx
+++ src/components/common/customElements/CustomButton.jsx
@@ -8,7 +8,7 @@ const CustomButton = ({ message, width }) => {
           type="submit"
           className={
             "flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" +
-            +" " +
+            " " +
             (width ? width : "w-full")
           }
         >
--- src/components/common/customElements/CustomLink.jsx
+++ src/components/common/customElements/CustomLink.jsx
@@ -4,16 +4,15 @@ import { Link } from "react-router-dom";
 const CustomLink = ({ message, to }) => {
   return (
     <>
-      <Link to={to}>
-        <div className="text-sm">
-          <a
-            href="#"
-            className="font-medium text-indigo-600 hover:text-indigo-500"
-          >
-            {message}
-          </a>
-        </div>
-      </Link>
+      <div className="text-sm">
+        <Link
+          to={to}
+          href="#"
+          className="font-medium text-indigo-600 hover:text-indigo-500"
+        >
+          {message}
+        </Link>
+      </div>
     </>
   );
 };

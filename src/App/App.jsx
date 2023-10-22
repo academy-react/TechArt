214c2a3b733c5c535a2caa651f9ccb5783a28e42
mr1370 <136124144+mr1370@users.noreply.github.com>
2023-10-22 17:13:03 +0330

    change app

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import { CourseSection } from "../components/Landing/CourseSlider/CourseSection";
-import { ProfessorCard } from "../components/ProfessorCard/ProfessorCard";
 
 
 function App() {
   return (
-    <>
-      <CourseSection/>
-      <ProfessorCard/>
-    </>
+   <></>
   );
 }
 

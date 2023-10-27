b4fe565f8dc48633c97c00fa6017983e427136de
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-10-27 18:36:21 +0330

    Course Detailes content stage one

--- src/components/CourseDetailContent/CourseDetailContent.jsx
+++ src/components/CourseDetailContent/CourseDetailContent.jsx
@@ -47,11 +47,7 @@ const userNavigation = [
   { name: "Your profile", href: "#" },
   { name: "Sign out", href: "#" },
 ];
-const tabs = [
-  { name: "Recently Viewed", href: "#", current: true },
-  { name: "Recently Added", href: "#", current: false },
-  { name: "Favorited", href: "#", current: false },
-];
+
 const files = [
   {
     name: "IMG_4985.HEIC",
@@ -108,45 +104,6 @@ export default function CourseDetailContent() {
         ```
       */}
       <div className="flex h-full">
-        {/* Narrow sidebar */}
-        <div className="hidden w-28 overflow-y-auto bg-indigo-700 md:block">
-          <div className="flex w-full flex-col items-center py-6">
-            <div className="flex flex-shrink-0 items-center">
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=white"
-                alt="Your Company"
-              />
-            </div>
-            <div className="mt-6 w-full flex-1 space-y-1 px-2">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className={classNames(
-                    item.current
-                      ? "bg-indigo-800 text-white"
-                      : "text-indigo-100 hover:bg-indigo-800 hover:text-white",
-                    "group w-full p-3 rounded-md flex flex-col items-center text-xs font-medium"
-                  )}
-                  aria-current={item.current ? "page" : undefined}
-                >
-                  <item.icon
-                    className={classNames(
-                      item.current
-                        ? "text-white"
-                        : "text-indigo-300 group-hover:text-white",
-                      "h-6 w-6"
-                    )}
-                    aria-hidden="true"
-                  />
-                  <span className="mt-2">{item.name}</span>
-                </a>
-              ))}
-            </div>
-          </div>
-        </div>
-
         {/* Mobile menu */}
         <Transition.Root show={mobileMenuOpen} as={Fragment}>
           <Dialog
@@ -248,111 +205,26 @@ export default function CourseDetailContent() {
 
         {/* Content area */}
         <div className="flex flex-1 flex-col overflow-hidden">
-          <header className="w-full">
-            <div className="relative z-10 flex h-16 flex-shrink-0 border-b border-gray-200 bg-white shadow-sm">
-              <button
-                type="button"
-                className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
-                onClick={() => setMobileMenuOpen(true)}
-              >
-                <span className="sr-only">Open sidebar</span>
-                <Bars3BottomLeftIcon className="h-6 w-6" aria-hidden="true" />
-              </button>
-              <div className="flex flex-1 justify-between px-4 sm:px-6">
-                <div className="flex flex-1">
-                  <form className="flex w-full md:ml-0" action="#" method="GET">
-                    <label htmlFor="desktop-search-field" className="sr-only">
-                      Search all files
-                    </label>
-                    <label htmlFor="mobile-search-field" className="sr-only">
-                      Search all files
-                    </label>
-                    <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                      <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-                        <MagnifyingGlassIcon
-                          className="h-5 w-5 flex-shrink-0"
-                          aria-hidden="true"
-                        />
-                      </div>
-                      <input
-                        name="mobile-search-field"
-                        id="mobile-search-field"
-                        className="h-full w-full border-transparent py-2 pl-8 pr-3 text-base text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:hidden"
-                        placeholder="Search"
-                        type="search"
-                      />
-                      <input
-                        name="desktop-search-field"
-                        id="desktop-search-field"
-                        className="hidden h-full w-full border-transparent py-2 pl-8 pr-3 text-base text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:block"
-                        placeholder="Search all files"
-                        type="search"
-                      />
-                    </div>
-                  </form>
-                </div>
-                <div className="ml-2 flex items-center space-x-4 sm:ml-6 sm:space-x-6">
-                  {/* Profile dropdown */}
-                  <Menu as="div" className="relative flex-shrink-0">
-                    <div>
-                      <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-                        <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80"
-                          alt=""
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {userNavigation.map((item) => (
-                          <Menu.Item key={item.name}>
-                            {({ active }) => (
-                              <a
-                                href={item.href}
-                                className={classNames(
-                                  active ? "bg-gray-100" : "",
-                                  "block px-4 py-2 text-sm text-gray-700"
-                                )}
-                              >
-                                {item.name}
-                              </a>
-                            )}
-                          </Menu.Item>
-                        ))}
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-
-                  <button
-                    type="button"
-                    className="flex items-center justify-center rounded-full bg-indigo-600 p-1 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                  >
-                    <PlusIconOutline className="h-6 w-6" aria-hidden="true" />
-                    <span className="sr-only">Add file</span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </header>
-
           {/* Main content */}
           <div className="flex flex-1 items-stretch overflow-hidden">
             <main className="flex-1 overflow-y-auto">
               <div className="mx-auto max-w-7xl px-4 pt-8 sm:px-6 lg:px-8">
                 <div className="flex">
-                  <h1 className="flex-1 text-2xl font-bold text-gray-900">
-                    Photos
-                  </h1>
+                  <p className="mt-6  text-md leading-8 text-gray-600 text-justify">
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و
+                    با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه
+                    و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی
+                    تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای
+                    کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و
+                    آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم
+                    افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص
+                    طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این
+                    صورت می توان امید داشت که تمام و دشواری موجود در ارائه
+                    راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل
+                    حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای
+                    موجود طراحی اساسا مورد استفاده قرار گیرد.
+                  </p>
+
                   <div className="ml-6 flex items-center rounded-lg bg-gray-100 p-0.5 sm:hidden">
                     <button
                       type="button"
@@ -371,69 +243,6 @@ export default function CourseDetailContent() {
                       />
                       <span className="sr-only">Use grid view</span>
                     </button>
-                  </div>
-                </div>
-
-                {/* Tabs */}
-                <div className="mt-3 sm:mt-2">
-                  <div className="sm:hidden">
-                    <label htmlFor="tabs" className="sr-only">
-                      Select a tab
-                    </label>
-                    {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-                    <select
-                      id="tabs"
-                      name="tabs"
-                      className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                      defaultValue="Recently Viewed"
-                    >
-                      <option>Recently Viewed</option>
-                      <option>Recently Added</option>
-                      <option>Favorited</option>
-                    </select>
-                  </div>
-                  <div className="hidden sm:block">
-                    <div className="flex items-center border-b border-gray-200">
-                      <nav
-                        className="-mb-px flex flex-1 space-x-6 xl:space-x-8"
-                        aria-label="Tabs"
-                      >
-                        {tabs.map((tab) => (
-                          <a
-                            key={tab.name}
-                            href={tab.href}
-                            aria-current={tab.current ? "page" : undefined}
-                            className={classNames(
-                              tab.current
-                                ? "border-indigo-500 text-indigo-600"
-                                : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                              "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"
-                            )}
-                          >
-                            {tab.name}
-                          </a>
-                        ))}
-                      </nav>
-                      <div className="ml-6 hidden items-center rounded-lg bg-gray-100 p-0.5 sm:flex">
-                        <button
-                          type="button"
-                          className="rounded-md p-1.5 text-gray-400 hover:bg-white hover:shadow-sm focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                        >
-                          <Bars4Icon className="h-5 w-5" aria-hidden="true" />
-                          <span className="sr-only">Use list view</span>
-                        </button>
-                        <button
-                          type="button"
-                          className="ml-0.5 rounded-md bg-white p-1.5 text-gray-400 shadow-sm focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-                        >
-                          <Squares2X2IconMini
-                            className="h-5 w-5"
-                            aria-hidden="true"
-                          />
-                          <span className="sr-only">Use grid view</span>
-                        </button>
-                      </div>
-                    </div>
                   </div>
                 </div>
 
@@ -467,6 +276,7 @@ export default function CourseDetailContent() {
                               "object-cover pointer-events-none"
                             )}
                           />
+
                           <button
                             type="button"
                             className="absolute inset-0 focus:outline-none"

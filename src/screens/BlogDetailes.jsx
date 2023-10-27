360baf7ce6cbd04b4fed981707b40494b7d6ebdf
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-10-27 21:53:29 +0330

    ....

--- src/screens/BlogDetailes.jsx
+++ src/screens/BlogDetailes.jsx
@@ -1,4 +1,3 @@
-import BlogDetailContent from "../components/BlogDetailes/BlogDetailContent";
 import { Footer } from "../components/common/Footer/Footer";
 import { NavbarHeader } from "../components/common/NavbarHeader/NavbarHeader";
 import TabsComponent from "../components/common/Tab/Tab";

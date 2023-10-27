619701a23f944787863b04861a2a46cdd2e148f8
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-10-28 01:46:53 +0330

    Course list

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import Courses from "../components/common/Courses/Courses";
 import Error404 from "../components/common/Error404.jsx/Error404";
+import Landing from "../components/common/Landing/Landing";
 import NewsArticle from "../components/common/NewsArticle/NewsArticle";
+import CourseList from "../components/common/StudentPannel/CourseList";
 import StudentPannel from "../components/common/StudentPannel/Studentpannel";
 import { BlogDetailes } from "../screens/BlogDetailes";
 import "./App.css";
@@ -12,7 +14,7 @@ const App = () => {
   return (
     <div className="	px-px w-auto 	">
       <div>
-        <Courses />
+        <CourseList />
       </div>
     </div>
   );

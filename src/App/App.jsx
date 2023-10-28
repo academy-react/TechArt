aca68592fc54f9014cb04716f42a1138a7b1ebc4
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-10-28 18:07:21 +0330

    rooting

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,35 +1,38 @@
 import React from "react";
-
+import { RouterProvider } from "react-router-dom";
+import { router } from "../Router/router";
+import { useState } from "react";
 import { CourseDetailes } from "../screens/CourseDetailes";
 import "./App.css";
 
 //fonts
 
-const App = () => {
-  return (
-    <div className="	px-px w-auto 	">
-      <div>
-        <CourseDetailes />
-      </div>
-    </div>
-  );
-};
-
-export default App;
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Auth />,
-//   },
-// ]);
-
-// function App() {
+// const App = () => {
 //   return (
-//     <>
-//       <RouterProvider router={router} />
-//     </>
+//     <div className="	px-px w-auto 	">
+//       <div>
+//         <CourseDetailes />
+//       </div>
+//     </div>
 //   );
-// }
+// };
 
 // export default App;
+/////
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CourseDetailes />,
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;

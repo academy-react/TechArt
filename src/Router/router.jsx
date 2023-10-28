8c88f7e0040a0e4b0cd81822eeabb885c7e0db41
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-10-29 03:14:16 +0330

    ....

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import { authRouter } from "./authRouter";
 
 const router = createBrowserRouter([...CourseDetailsRouter]);
 

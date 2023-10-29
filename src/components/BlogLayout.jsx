d6accb57bd327dd2f9c358b18ffc565affa70a5d
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-10-30 01:20:34 +0330

    ...

--- src/components/BlogLayout.jsx
+++ src/components/BlogLayout.jsx
@@ -1,7 +1,9 @@
 function BlogLayout({ children }) {
   return (
     <>
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">{children}</div>
+      <div className="container mx-auto px-4 sm:px-6 lg:px-8 border-2 border-red-500">
+        {children}
+      </div>
     </>
   );
 }

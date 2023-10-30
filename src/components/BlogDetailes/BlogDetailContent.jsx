5562c155a54e348733a4206f93301da28d78e687
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-10-31 01:45:56 +0330

    sidebar

--- src/components/BlogDetailes/BlogDetailContent.jsx
+++ src/components/BlogDetailes/BlogDetailContent.jsx
@@ -1,43 +1,23 @@
+import image from "../../assets/image/BlogDetailes/news.jpg";
+
 export default function BlogDetailContent() {
   return (
-    <div className="bg-indigo-800">
-      <div className="mx-auto max-w-7xl py-12 px-6 sm:py-16 lg:px-8 lg:py-20">
-        <div className="mx-auto max-w-4xl text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Trusted by developers from over 80 planets
-          </h2>
-          <p className="mt-3 text-xl text-indigo-200 sm:mt-4">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Repellendus repellat laudantium.
-          </p>
-        </div>
-        <dl className="mt-10 text-center sm:mx-auto sm:grid sm:max-w-3xl sm:grid-cols-3 sm:gap-8">
-          <div className="flex flex-col">
-            <dt className="order-2 mt-2 text-lg font-medium leading-6 text-indigo-200">
-              Pepperoni
-            </dt>
-            <dd className="order-1 text-5xl font-bold tracking-tight text-white">
-              100%
-            </dd>
-          </div>
-          <div className="mt-10 flex flex-col sm:mt-0">
-            <dt className="order-2 mt-2 text-lg font-medium leading-6 text-indigo-200">
-              Delivery
-            </dt>
-            <dd className="order-1 text-5xl font-bold tracking-tight text-white">
-              24/7
-            </dd>
-          </div>
-          <div className="mt-10 flex flex-col sm:mt-0">
-            <dt className="order-2 mt-2 text-lg font-medium leading-6 text-indigo-200">
-              Calories
-            </dt>
-            <dd className="order-1 text-5xl font-bold tracking-tight text-white">
-              100k+
-            </dd>
-          </div>
-        </dl>
+    <div className="flex-col ">
+      <div className="rounded-xl overflow-hidden">
+        <img className="" src={image} alt="" />
       </div>
+      <p className="mt-6  text-md leading-8 text-gray-600 text-justify">
+        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+        از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
+        سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای
+        متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه
+        درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با
+        نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان
+        خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید
+        داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان
+        رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات
+        پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+      </p>
     </div>
   );
 }

dc7e4b2258c15b345e8518ebc63ccbfc5e479d0c
Cloner <aliabdi1108@gmail.com>
2023-10-30 17:11:08 +0330

    pagin

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import { authRouter } from "./authRouter";
+import { CourseList } from "../components/StudentPanel/CourseList";
 
-const router = createBrowserRouter([...authRouter]);
+const router = createBrowserRouter([
+  { path: "/", element: <CourseList /> },
+  ...authRouter,
+]);
 
 export { router };

73c01d1106f8fc1111cfd884d60ef3394ba84193
mr1370 <136124144+mr1370@users.noreply.github.com>
2023-10-31 10:26:02 +0330

    card change

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,12 +1,15 @@
 import { Courses } from "../components/Courses/Courses";
+import { CourseSection } from "../components/Landing/CourseSlider/CourseSection";
+import { ProfessorCard } from "../components/ProfessorCard/ProfessorCard";
 import "./App.css";
 
 
 function App() {
   return (
    <>
-   <Courses/>
-   </>
+   <CourseSection/>
+   <ProfessorCard/>
+  </>
   );
 }
 

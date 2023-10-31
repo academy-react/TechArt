33d0363714b4a4eadd73eb16fece212fc62ad65a
Cloner <aliabdi1108@gmail.com>
2023-10-31 15:11:56 +0330

    end

--- src/components/StudentPanel/CourseList/TableInnerDL.jsx
+++ src/components/StudentPanel/CourseList/TableInnerDL.jsx
@@ -4,9 +4,7 @@ const TableInnerDL = ({ label, toShow, stackSize }) => {
   return (
     <>
       <dt className="sr-only hidden">{label}</dt>
-      <dd
-        className={"mt-1 truncate text-gray-500" + " " + stackSize + ":hidden"}
-      >
+      <dd className={"mt-1 truncate text-gray-500" + " " + stackSize}>
         <span className=" text-gray-700">{label}:</span> {toShow}
       </dd>
     </>
--- src/components/common/Pagination.jsx
+++ src/components/common/Pagination.jsx
@@ -1,70 +1,59 @@
 import React from "react";
 import ReactPaginate from "react-paginate";
+import { IoChevronBackOutline, IoChevronForwardOutline } from "react-icons/io5";
 
 const Pagination = ({ handlePageClick, pageCount, currentPage }) => {
   const renderNext = () => {
     return (
-      <span class="relative inline-flex items-center rounded-l-md px-2 py-3 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
-        <span class="sr-only">Previous</span>
-        <svg
-          class="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
-      </span>
+      <>
+        <span className="sr-only">Previous</span>
+        <IoChevronBackOutline className="h-5 w-5" aria-hidden="true" />
+      </>
     );
   };
   const renderPrev = () => {
     return (
-      <a
-        href="#"
-        class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-      >
-        <span class="sr-only">Next</span>
-        <svg
-          class="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z"
-            clip-rule="evenodd"
-          ></path>
-        </svg>
-      </a>
+      <>
+        {" "}
+        <span className="sr-only">Next</span>
+        <IoChevronForwardOutline className="h-5 w-5" aria-hidden="true" />
+      </>
     );
   };
   return (
     <>
-      <nav
-        class="isolate inline-flex -space-x-px rounded-md shadow-sm"
-        aria-label="Pagination"
-      ></nav>
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel={renderNext()}
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={3}
-        pageCount={pageCount}
-        previousLabel={renderPrev()}
-        renderOnZeroPageCount={null}
-        forcePage={currentPage}
-        containerClassName="isolate inline-flex -space-x-px rounded-md shadow-sm"
-        activeClassName={
-          "relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        }
-        pageClassName="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-        breakClassName="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
-      />
+      <div className="flex flex-col sm:flex-row items-center sm:justify-between bg-white py-3 sm:px-8 ">
+        <nav
+          class="isolate inline-flex -space-x-px rounded-md shadow-sm"
+          aria-label="Pagination"
+        >
+          <ReactPaginate
+            breakLabel="..."
+            nextLabel={renderNext()}
+            onPageChange={handlePageClick}
+            pageRangeDisplayed={2}
+            pageCount={pageCount}
+            previousLabel={renderPrev()}
+            renderOnZeroPageCount={null}
+            forcePage={currentPage}
+            containerClassName="isolate inline-flex -space-x-px rounded-md shadow-sm"
+            activeLinkClassName={
+              "relative z-10 inline-flex items-center !bg-indigo-600 px-4 py-2 text-sm font-semibold text-white "
+            }
+            pageLinkClassName="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
+            breakClassName="relative inline-flex items-center border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700"
+            previousLinkClassName="relative inline-flex items-center rounded-r-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
+            nextLinkClassName="relative inline-flex items-center rounded-l-md border border-gray-300 bg-white px-2 py-2 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20"
+          />
+        </nav>
+        <div>
+          <p className="text-sm text-gray-700 pt-4 sm:py-0">
+            {"درحال نمایش"} <span className="font-medium">1</span> {"تا"}{" "}
+            <span className="font-medium">10</span> {"از"}{" "}
+            <span className="font-medium">97</span> {"نتیجه"}
+          </p>
+        </div>
+      </div>
     </>
   );
 };

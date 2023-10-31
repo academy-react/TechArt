587683ea305a95af5ff10353b3de091cc8b71eaa
Cloner <aliabdi1108@gmail.com>
2023-10-31 16:21:55 +0330

    aa

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -2,11 +2,15 @@ import { createBrowserRouter } from "react-router-dom";
 import { BlogDetailsRouter } from "./BlogDetailsRouter";
 import { authRouter } from "./authRouter";
 import { CourseDetailsRouter } from "./CourseDetailsRouter";
-
+import { Landing } from "../screens/Landing/Landing";
 const router = createBrowserRouter([
   ...authRouter,
   ...CourseDetailsRouter,
   ...BlogDetailsRouter,
+  {
+    path: "/",
+    element: <Landing />,
+  },
 ]);
 
 export { router };

119d886c4b9164e30389d654bab23c525ecbee88
maryamfiroozmandi <maryam.firoozmandi@gmail.com>
2023-11-02 13:54:30 +0330

    SSS

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,9 @@ import App from "./App/App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([
-  { path:"/", element: <App/> },
-  { path:"/dashboard", element: <App/> },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );

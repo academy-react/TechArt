cdf749831f77cacf1c84a9f1ef8f495960ccea66
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-11-05 00:16:30 +0330

    bug fixed

--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import { authRouter } from "./authRouter";
-import { RouterProvider } from "react-router-dom";
-import { router } from "../Router/router";
 import { CourseDetailsRouter } from "./CourseDetailsRouter";
-
-import { BlogDetailsRouter } from "./BlogDetailsRouter";
 
 const router = createBrowserRouter([...CourseDetailsRouter]);
 

6adf9eb62d1647917822f6f8dc0b8ec46c35eeb4
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-11-13 18:09:58 +0330

    ljkbjb

--- src/components/common/customElements/customTab.jsx
+++ src/components/common/customElements/customTab.jsx
@@ -13,7 +13,7 @@ export default function TabsComponent() {
                 href="#"
                 onClick={() => setOpenTab(1)}
                 className={` ${
-                  openTab === 1 ? "bg-purple-600 text-blue" : ""
+                  openTab === 1 ? "bg-purple-600 text-white" : ""
                 } inline-block px-4 py-2 text-gray-600 bg-white rounded shadow`}
               >
                 React Tabs 1
@@ -24,25 +24,34 @@ export default function TabsComponent() {
                 href="#"
                 onClick={() => setOpenTab(2)}
                 className={` ${
-                  openTab === 2 ? "bg-purple-600 text-blue" : ""
+                  openTab === 2 ? "bg-purple-600 text-white" : ""
                 } inline-block px-4 py-2 text-gray-600 bg-white rounded shadow`}
               >
                 React Tabs 2
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                onClick={() => setOpenTab(3)}
+                className={` ${
+                  openTab === 3 ? "bg-purple-600 text-white" : ""
+                } inline-block px-4 py-2 text-gray-600 bg-white rounded shadow`}
+              >
+                React Tabs 3
               </a>
             </li>
           </ul>
           <div className="p-3 mt-6 bg-white border">
             <div className={openTab === 1 ? "block" : "hidden"}>
               {" "}
-              این دوره که به صورت جامع کتابخانه React را آموزش داده است سعی شده
-              است که هم مفاهیم تئوری و هم مفاهیم عملی و کدنویسی به صورت کامل
-              گفته شود. بعد از گذراندن و درک مفاهیم گفته شده این دوره آموزشی شما
-              می توانید خود را یک front-end developer بدانید که چند پروژه انجام
-              داده است و آماده است که به عنوان React developer وارد بازار کار
-              شود.
+              React JS with Tailwind CSS Tab 1 Content show
             </div>
             <div className={openTab === 2 ? "block" : "hidden"}>
               React JS with Tailwind CSS Tab 2 Content show
+            </div>
+            <div className={openTab === 3 ? "block" : "hidden"}>
+              React JS with Tailwind CSS Tab 3 Content show
             </div>
           </div>
         </div>

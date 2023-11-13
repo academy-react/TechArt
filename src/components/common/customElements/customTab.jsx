19b0a688bc880c630f5049ff2121ff2486226f5f
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-11-13 17:41:32 +0330

    xxxx

--- src/components/common/customElements/customTab.jsx
+++ src/components/common/customElements/customTab.jsx
@@ -1,7 +1,4 @@
 import { useState } from "react";
-import { CustomForm } from "./CustomForm";
-
-import Rate from "./Rate";
 
 export default function TabsComponent() {
   const [openTab, setOpenTab] = useState(1);
@@ -9,17 +6,17 @@ export default function TabsComponent() {
   return (
     <div>
       <div className="container mx-auto mt-12">
-        <div className="flex flex-col items-start justify-center  ">
+        <div className="flex flex-col items-center justify-center max-w-xl">
           <ul className="flex space-x-2">
             <li>
               <a
                 href="#"
                 onClick={() => setOpenTab(1)}
                 className={` ${
-                  openTab === 1 ? "bg-purple-600 text-blue" : ""
+                  openTab === 1 ? "bg-purple-600 text-white" : ""
                 } inline-block px-4 py-2 text-gray-600 bg-white rounded shadow`}
               >
-                توضیحات دوره
+                React Tabs 1
               </a>
             </li>
             <li>
@@ -27,28 +24,34 @@ export default function TabsComponent() {
                 href="#"
                 onClick={() => setOpenTab(2)}
                 className={` ${
-                  openTab === 2 ? "bg-purple-600 text-blue" : ""
+                  openTab === 2 ? "bg-purple-600 text-white" : ""
                 } inline-block px-4 py-2 text-gray-600 bg-white rounded shadow`}
               >
-                ثبت نظر
+                React Tabs 2
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                onClick={() => setOpenTab(3)}
+                className={` ${
+                  openTab === 3 ? "bg-purple-600 text-white" : ""
+                } inline-block px-4 py-2 text-gray-600 bg-white rounded shadow`}
+              >
+                React Tabs 3
               </a>
             </li>
           </ul>
           <div className="p-3 mt-6 bg-white border">
             <div className={openTab === 1 ? "block" : "hidden"}>
               {" "}
-              <p className="mt-6  text-md leading-8 text-gray-600 text-justify">
-                این دوره که به صورت جامع کتابخانه React را آموزش داده است سعی
-                شده است که هم مفاهیم تئوری و هم مفاهیم عملی و کدنویسی به صورت
-                کامل گفته شود. بعد از گذراندن و درک مفاهیم گفته شده این دوره
-                آموزشی شما می توانید خود را یک front-end developer بدانید که چند
-                پروژه انجام داده است و آماده است که به عنوان React developer
-                وارد بازار کار شود.
-              </p>
-              <Rate />
+              React JS with Tailwind CSS Tab 1 Content show
             </div>
             <div className={openTab === 2 ? "block" : "hidden"}>
-              <CustomForm>xxx</CustomForm>
+              React JS with Tailwind CSS Tab 2 Content show
+            </div>
+            <div className={openTab === 3 ? "block" : "hidden"}>
+              React JS with Tailwind CSS Tab 3 Content show
             </div>
           </div>
         </div>

3336c326d218938a486cc17d7304bcfa4972bef1
maryamfiroozmandi <maryam.firoozmandi@gmail.com>
2023-11-16 14:33:44 +0330

    api

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,43 +5,45 @@ import React from "react"
 import image from "../../assets/image/Annotation .png";
 
 
-const people = [
-        {
-        name: 'لورم ایسپوم دارای استاندارد صنعتی متن ساختگی از سال میلادی زمانی که یک پرینتر ناشناخته به زحمت 1500 درحال گسترش بود می باشد ',
-        },
-        {
-        name: 'لورم ایسپوم دارای استاندارد صنعتی متن ساختگی از سال میلادی زمانی که یک پرینتر ناشناخته به زحمت 1500 درحال گسترش بود می باشد ',        
-        },
-        {
-        name: 'لورم ایسپوم دارای استاندارد صنعتی متن ساختگی از سال میلادی زمانی که یک پرینتر ناشناخته به زحمت 1500 درحال گسترش بود می باشد ',
-        },
-        {
-        name: 'لورم ایسپوم دارای استاندارد صنعتی متن ساختگی از سال میلادی زمانی که یک پرینتر ناشناخته به زحمت 1500 درحال گسترش بود می باشد ',
-        },
-        {
-        name: 'لورم ایسپوم دارای استاندارد صنعتی متن ساختگی از سال میلادی زمانی که یک پرینتر ناشناخته به زحمت 1500 درحال گسترش بود می باشد ',
-        },
-        {
-        name: 'لورم ایسپوم دارای استاندارد صنعتی متن ساختگی از سال میلادی زمانی که یک پرینتر ناشناخته به زحمت 1500 درحال گسترش بود می باشد ',
-        },
-    ]
+// const people = [
+//         {
+//         name: 'لورم ایسپوم دارای استاندارد صنعتی متن ساختگی از سال میلادی زمانی که یک پرینتر ناشناخته به زحمت 1500 درحال گسترش بود می باشد ',
+//         },
+//         {
+//         name: 'لورم ایسپوم دارای استاندارد صنعتی متن ساختگی از سال میلادی زمانی که یک پرینتر ناشناخته به زحمت 1500 درحال گسترش بود می باشد ',        
+//         },
+//         {
+//         name: 'لورم ایسپوم دارای استاندارد صنعتی متن ساختگی از سال میلادی زمانی که یک پرینتر ناشناخته به زحمت 1500 درحال گسترش بود می باشد ',
+//         },
+//         {
+//         name: 'لورم ایسپوم دارای استاندارد صنعتی متن ساختگی از سال میلادی زمانی که یک پرینتر ناشناخته به زحمت 1500 درحال گسترش بود می باشد ',
+//         },
+//         {
+//         name: 'لورم ایسپوم دارای استاندارد صنعتی متن ساختگی از سال میلادی زمانی که یک پرینتر ناشناخته به زحمت 1500 درحال گسترش بود می باشد ',
+//         },
+//         {
+//         name: 'لورم ایسپوم دارای استاندارد صنعتی متن ساختگی از سال میلادی زمانی که یک پرینتر ناشناخته به زحمت 1500 درحال گسترش بود می باشد ',
+//         },
+//     ]
     
-    const Card = () => {
+    const Card = ({cardList}) => {
         return (
         <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 p-5">
-            {people.map((person) => (
+            {cardList.map((person) => (
             <div
-                key={person.email}
+                key={person}
                 className="relative flex flex-row  gap-4 rounded-lg border border-gray-300 bg-white p-3 shadow-sm focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:border-gray-400"
             >
                 <div className=" ">
-                <img className="w-56 h-full object-cover  mx-auto  "  src={image} alt="" />
+                <img className="object-cover  mx-auto  "  src={person.currentImageAddressTumb} alt="" />
 
                 </div>
                 <div className="min-w-0 grid grid-cols-1 md:grid-cols-1 ">
                 <a href="#" className="focus:outline-none">
                     <span className="absolute inset-0" aria-hidden="true" />
-                    <p className=" text-gray-900 ">{person.name}</p>
+                    <p className=" text-gray-900 ">{person.addUserFullName}</p>
+                    <p>{person.updateDate}</p>
+                    <p>{person.insertDate}</p>
                 </a>
                 </div>
             </div>
@@ -49,9 +51,6 @@ const people = [
         </div>
         )
     }
-    
-
-    
     
     export {Card}
 
--- src/components/Card/CardHolder.jsx
+++ src/components/Card/CardHolder.jsx
@@ -1,18 +1,34 @@
 
 
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 
 import { Card } from './Card'
 import { CardTitle } from './CardTitle'
+import axios from 'axios';
 
 
 const CardHolder = () => {
+    const[cardList,setCardList] = useState([]);
+
+    const getCard = async () => {
+        console.log('fetching......');
+        const result = await axios.get(
+            "https://acadapi.etacorealtime.ir/api/News?PageNumber=1&RowsOfPage=10&SortingCol=InsertDate&SortType=DESC&Query="
+        );
+    console.log(result.data);
+    setCardList(result.data.news);
+    };
+    useEffect(() =>{
+        getCard();
+    }, []);
+
+    console.log(cardList)
+    
     return (
         <Fragment>
-            <div className=''>
                 <CardTitle />
-                <Card />
-            </div>
+                <Card cardList={cardList}/>
+            
         </Fragment>
     )
 }

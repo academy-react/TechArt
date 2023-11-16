42779b89be08b9578e1e6aabf325feb08fc326ff
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-11-16 15:18:59 +0330

    ...

--- src/components/Card/CardHolder.jsx
+++ src/components/Card/CardHolder.jsx
@@ -1,40 +1,35 @@
+import React, { Fragment, useEffect, useState } from "react";
 
-
-import React, { Fragment, useEffect, useState } from 'react'
-
-import { Card } from './Card'
-import { CardTitle } from './CardTitle'
-import axios from 'axios';
-import { baseUrl } from '../../config';
-
-
-
+import { Card } from "./Card";
+import { CardTitle } from "./CardTitle";
+import axios from "axios";
+import { baseUrl } from "../../config";
 
 const CardHolder = () => {
-    const[cardList,setCardList] = useState([]);
+  const [cardList, setCardList] = useState([]);
 
-    const getCard = async () => {
-        console.log('fetching......');
-        const result = await axios.get(
-            `${baseUrl}/News?PageNumber=1&RowsOfPage=10&SortingCol=InsertDate&SortType=DESC&Query=`
-        );
+  const getCard = async () => {
+    console.log("fetching......");
+    const result = await axios.get(
+      `${baseUrl}/News?PageNumber=1&RowsOfPage=10&SortingCol=InsertDate&SortType=DESC&Query=`
+    );
     console.log(result.data);
-     setCardList(result.data.news);
-    };
-    useEffect(() =>{
-        getCard();
-    }, []);
+    setCardList(result.data.news);
+  };
+  useEffect(() => {
+    getCard();
+  }, []);
 
-    console.log(cardList)
-    
-    return (
-        <Fragment>
-            <div className='' >
-                <CardTitle />
-                <Card cardList={cardList}/>
-            </div>
-        </Fragment>
-    )
-}
+  console.log(cardList);
 
-export {CardHolder}
+  return (
+    <Fragment>
+      <div className="">
+        <CardTitle />
+        <Card cardList={cardList} />
+      </div>
+    </Fragment>
+  );
+};
+
+export { CardHolder };

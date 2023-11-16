20c446f5b05c337fb2ea644e7d8ea9df011d4530
maryamfiroozmandi <maryam.firoozmandi@gmail.com>
2023-11-16 18:54:59 +0330

    api

--- src/components/News/CardHolder.jsx
+++ src/components/News/CardHolder.jsx
@@ -1,17 +1,30 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { Card } from './Card'
+import axios from 'axios';
 
 const CardHolder = () => {
     const[newCard,setNewCard] = useState([]);
 
+    const getCardNews = async () => {
+        console.log('fetching......');
+        const result = await axios.get(
+            `${baseUrl}/News?PageNumber=1&RowsOfPage=10&SortingCol=InsertDate&SortType=DESC&Query=`
+        );
+    console.log(result.data);
+    setNewCard(result.data.news);
+    };
+    useEffect(() =>{
+        getCardNews();
+    }, []);
 
+    console.log(newCard)
 
 
     return (
         <div className="CardHolder">
-                <Card />
+                <Card newCard={newCard}/>
             
         </div>
     )

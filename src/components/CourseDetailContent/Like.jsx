6029e4502c204ddf30c4f4dca1872dbc8bdcd3f7
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-12-07 21:38:43 +0330

    add comment api

--- src/components/CourseDetailContent/Like.jsx
+++ src/components/CourseDetailContent/Like.jsx
@@ -10,38 +10,43 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from "react-toastify";
 
 const LikeButton = () => {
-  const [isLiked, setIsLiked] = useState(false);
-
-  // Replace these values with the actual courseId and authToken
-  const courseId = "7b41aed7-2576-ee11-b6c7-ca6d3e095898"; // Replace with the actual course ID
+  const courseId = "7b41aed7-2576-ee11-b6c7-ca6d3e095898";
   const authToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3OTJkMjQ5My0yMjA3LTRkZGYtYTc4OS03MzE1YTBkMTFhZTIiLCJqdGkiOiJlY2QwNDI0MC1hZTBlLTQ0NTItYmI4Yi0xMzBiNGQ3NjcyNjMiLCJlbWFpbCI6Im1hc2cxMzc3QGdtYWlsLmNvbSIsIlVpZCI6IkM3Ui8vMTJMTEpORHMwVHJIcEZvWDg2RTJQbTdjUmJlam5wZlZmU0tnbkk9RXM3ODg5OGQ5NjllZWY2ZWNhZDNjMjlhM2E2MjkyODBlNjg2Y2YwYzNmNWQ1YTg2YWZmM2NhMTIwMjBjOTIzYWRjNmM5MiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6WyJTdHVkZW50IiwiQWRtaW5pc3RyYXRvciIsIlRlYWNoZXIiLCJSZWZlcmVlIiwiVG91cm5hbWVudE1lbnRvciJdLCJleHAiOjE3MDE5ODEwNTgsImlzcyI6IlNlcGVockFjYWRlbXkiLCJhdWQiOiJTZXBlaHJBY2FkZW15In0.xjJoVhQUVKMF4yVHVmToY3UuxfdU_hRD4o1cEgnApwg";
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI3OTJkMjQ5My0yMjA3LTRkZGYtYTc4OS03MzE1YTBkMTFhZTIiLCJqdGkiOiJlY2QwNDI0MC1hZTBlLTQ0NTItYmI4Yi0xMzBiNGQ3NjcyNjMiLCJlbWFpbCI6Im1hc2cxMzc3QGdtYWlsLmNvbSIsIlVpZCI6IkM3Ui8vMTJMTEpORHMwVHJIcEZvWDg2RTJQbTdjUmJlam5wZlZmU0tnbkk9RXM3ODg5OGQ5NjllZWY2ZWNhZDNjMjlhM2E2MjkyODBlNjg2Y2YwYzNmNWQ1YTg2YWZmM2NhMTIwMjBjOTIzYWRjNmM5MiIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvcm9sZSI6WyJTdHVkZW50IiwiQWRtaW5pc3RyYXRvciIsIlRlYWNoZXIiLCJSZWZlcmVlIiwiVG91cm5hbWVudE1lbnRvciJdLCJleHAiOjE3MDE5ODEwNTgsImlzcyI6IlNlcGVockFjYWRlbXkiLCJhdWQiOiJTZXBlaHJBY2FkZW15In0.xjJoVhQUVKMF4yVHVmToY3UuxfdU_hRD4o1cEgnApwg"; // Replace with your actual auth token
+
+  const [isLiked, setLiked] = useState(false);
+
   const handleLikeClick = async () => {
     try {
-      // Make API call to like/unlike course using Axios
-      const response = await axios.post(
-        `${baseUrl}/Course/AddCourseLike?CourseId=${courseId}`,
-        {},
-        {
+      if (isLiked) {
+        // Unlike
+        await axios.delete(`${baseUrl}/Course/DeleteCourseLike`, {
           headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${authToken}`, // Include the authorization token in the header
-            // Include any additional headers if required
+            Authorization: `Bearer ${authToken}`,
           },
-        }
-      );
-
-      if (response.status === 200) {
-        // API call successful
-        setIsLiked((prev) => !prev); // Toggle the like state
-        toast.success("Course liked!"); // Show success toast
+          data: { courseId: courseId }, // Send courseId in the request body
+        });
       } else {
-        // API call failed
-        toast.error("Failed to like course. Please try again."); // Show error toast
+        // Like
+        await axios.post(
+          `${baseUrl}/Course/AddCourseLike?CourseId=${courseId}`,
+          {},
+          {
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+          }
+        );
       }
+
+      // Update the like state or perform any other necessary actions
+      setLiked(!isLiked);
+      const message = isLiked ? "Disliked!" : "Liked!";
+      toast.info(message);
     } catch (error) {
-      console.error("Error making API call:", error);
-      toast.error("An error occurred. Please try again."); // Show error toast
+      console.error("Error making API request:", error);
+      // Handle error as needed
+      toast.error("Failed to perform action. Please try again.");
     }
   };
 
@@ -53,7 +58,7 @@ const LikeButton = () => {
       onClick={handleLikeClick}
     >
       <FaHeart className="mr-2" />
-      <span>{isLiked ? "Liked!" : ""}</span>
+      <span>{isLiked ? "Liked!" : "Like"}</span>
       <ToastContainer position="bottom-right" />
     </div>
   );
--- src/components/common/ContactForm.jsx
+++ src/components/common/ContactForm.jsx
@@ -1,62 +1,131 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
-
 import CardHeading from "./CardHeading";
+import { baseUrl } from "../../config";
+import React, { useState } from "react";
+import axios from "axios";
+
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function ContactForm() {
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    Title: "Titlleeeeee masg",
+    CourseId: "7b41aed7-2576-ee11-b6c7-ca6d3e095898",
+    Describe: "sadsasasadsadasdsadadsadaasd",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post(`${baseUrl}/Course/AddCommentCourse`, formData);
+
+      // Show success message
+      toast.success("Comment submitted successfully!", {
+        position: "bottom-right",
+      });
+
+      // You may add additional logic here after a successful API call
+    } catch (error) {
+      console.error("Error submitting comment:", error);
+
+      // Show error message
+      toast.error("Failed to submit comment. Please try again.", {
+        position: "bottom-right",
+      });
+    }
+  };
+
   return (
     <div className="relative mx-auto max-w-xl">
-      <CardHeading />
-      <div className="mt-6">
-        <form
-          action="#"
-          method="POST"
-          className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
-        >
-          <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block text-sm font-medium text-gray-700"
-            >
-              پیام شما
-            </label>
-            <div className="mt-1">
-              <textarea
-                id="message"
-                name="message"
-                rows={4}
-                className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-lg focus:border-indigo-500 focus:ring-indigo-500"
-                defaultValue={""}
-              />
-            </div>
+      <ToastContainer position="bottom-right" />
+      <form
+        onSubmit={handleFormSubmit}
+        className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+      >
+        <div className="sm:col-span-2">
+          <label
+            htmlFor="message"
+            className="block text-sm font-medium text-gray-700"
+          >
+            پیام شما
+          </label>
+          <div className="mt-1">
+            <textarea
+              id="message"
+              name="Describe"
+              rows={4}
+              value={formData.Describe}
+              onChange={handleInputChange}
+              className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-lg focus:border-indigo-500 focus:ring-indigo-500"
+            />
           </div>
+        </div>
 
-          <div className="sm:col-span-2">
-            <button
-              type="submit"
-              className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            >
-              ثبت نظر
-            </button>
-          </div>
-        </form>
-      </div>
+        <div className="sm:col-span-2">
+          <button
+            type="submit"
+            className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            ثبت نظر
+          </button>
+        </div>
+      </form>
     </div>
   );
-}
+};
+
+export default ContactForm;
+
+// export default function ContactForm() {
+//   return (
+//     <div className="relative mx-auto max-w-xl">
+//       <CardHeading />
+//       <div className="mt-6">
+//         <form
+//           action="#"
+//           method="POST"
+//           className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8"
+//         >
+//           <div className="sm:col-span-2">
+//             <label
+//               htmlFor="message"
+//               className="block text-sm font-medium text-gray-700"
+//             >
+//               پیام شما
+//             </label>
+//             <div className="mt-1">
+//               <textarea
+//                 id="message"
+//                 name="message"
+//                 rows={4}
+//                 className="block w-full rounded-md border-gray-300 py-3 px-4 shadow-lg focus:border-indigo-500 focus:ring-indigo-500"
+//                 defaultValue={""}
+//               />
+//             </div>
+//           </div>
+
+//           <div className="sm:col-span-2">
+//             <button
+//               type="submit"
+//               className="inline-flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+//             >
+//               ثبت نظر
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }

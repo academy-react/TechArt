1932a87c04cbdde0d494ada2652f18fae2df2319
M.Hashemi <malihe.hashemi2020@gmail.com>
2023-12-12 21:44:01 +0330

    bug fixed

--- src/components/News/CardHolder.jsx
+++ src/components/News/CardHolder.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { FiSearch } from "react-icons/fi";
 
 import { Card } from "./Card";
+import { baseUrl } from "../../config";
 
 const CardHolder = () => {
   const [newCard, setNewCard] = useState([]);

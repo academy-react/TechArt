81da0992997bcffbd4c046fdfe01651c54127c4a
M.Hashemi <malihe.hashemi2020@gmail.com>
2024-03-06 23:15:49 +0330

    2222

--- src/components/Auth/SignIn.jsx
+++ src/components/Auth/SignIn.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { SVGSection } from "./SVGSection";
 import { FormSection } from "./FormSection";
 import { AuthLink } from "./AuthCustomElement";
-import SignInSVG from "../../assets/image/SignIn.svg";
+// import SignInSVG from "../../assets/image/SignIn.svg";
 import { signInSchema } from "../../core/validations/schemas/auth";
 import {
   CustomButton,
@@ -23,33 +23,44 @@ function SignIn() {
 
   return (
     <>
-      <SVGSection SVGSrc={SignInSVG} alt={"Sign In SVG"}></SVGSection>
-      <FormSection>
-        <AuthHeading message={"به اکانت کاربری خود وارد شوید "} />
-        <CustomForm
-          initialValues={{ email: "", password: "" }}
-          validationSchema={signInSchema}
-          onSubmit={onSubmit}
-        >
-          <CustomInput name={"email"} label={"آدرس ایمیل"} type={"text"}>
-            <IoMail />
-          </CustomInput>
+      <main className="max-w-5xl">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+          {/* <SVGSection SVGSrc={SignInSVG} alt={"Sign In SVG"}></SVGSection> */}
+          <FormSection>
+            <AuthHeading message={"به اکانت کاربری خود وارد شوید "} />
+            <CustomForm
+              initialValues={{ email: "", password: "" }}
+              validationSchema={signInSchema}
+              onSubmit={onSubmit}
+            >
+              <CustomInput name={"email"} label={"آدرس ایمیل"} type={"text"}>
+                <IoMail />
+              </CustomInput>
 
-          <CustomInput name={"password"} label={"رمز عبور"} type={"password"}>
-            <IoLockClosed />
-          </CustomInput>
+              <CustomInput
+                name={"password"}
+                label={"رمز عبور"}
+                type={"password"}
+              >
+                <IoLockClosed />
+              </CustomInput>
 
-          <CustomCheckBox name={"rememberMe"} message={"مرا به خاطر بسپار"} />
-          <CustomLink
-            to={"forgetPass"}
-            message={"رمز عبورت رو  فراموش کردی؟"}
-          />
+              <CustomCheckBox
+                name={"rememberMe"}
+                message={"مرا به خاطر بسپار"}
+              />
+              <CustomLink
+                to={"forgetPass"}
+                message={"رمز عبورت رو  فراموش کردی؟"}
+              />
 
-          <CustomLink to={"register"} message={"هنوز عضو سایت نشدی؟!"} />
+              <CustomLink to={"register"} message={"هنوز عضو سایت نشدی؟!"} />
 
-          <CustomButton message={"ورود"} />
-        </CustomForm>
-      </FormSection>
+              <CustomButton message={"ورود"} />
+            </CustomForm>
+          </FormSection>
+        </div>
+      </main>
     </>
   );
 }
